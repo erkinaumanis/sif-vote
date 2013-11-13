65949300febb7012e2736d10a0e2ccafd5836cea
Evan Casey <evan.casey92@gmail.com>
2013-11-12 18:09:02 -0800

    mongo + new pitches

---
+++ app/views.py
@@ -0,0 +1,81 @@
+from datetime import datetime, timedelta
+from flask import Flask, request, redirect, render_template, session, url_for, send_from_directory, jsonify
+from wtforms import Form, BooleanField, TextField, validators, ValidationError
+from models import get_all_pitches, create_pitch, create_action, get_pitch_actions, get_all_actions, get_all_pitches
+# from lib import forms, process
+from app import app
+import pdb
+
+@app.route('/', methods=['GET'])
+def index():
+    pitches = get_all_pitches()
+    return render_template('list.html', stocks=pitches)
+
+# Renders page for new pitches
+@app.route('/new', methods=['GET'])
+def new():
+    return render_template('new.html')
+
+# Takes in form data for new pitches
+@app.route('/create', methods=['POST'])
+def create():
+    if request.method == "POST":
+        data = request.form
+
+        # add new pitches and actions to mongo
+        create_pitch(data['pitch-name'], data['ticker'], data['pitch-date'])
+        create_action(data['symbol-1'], data['pitch-name'], data['action-1'], data['amount-1'], data['ticker'])
+        create_action(data['symbol-2'], data['pitch-name'], data['action-2'], data['amount-2'], data['ticker'])
+
+        pitches = get_all_pitches()
+        
+        return render_template('list.html', stocks=pitches)
+
+
+@app.route('/list', methods=['GET'])
+def get_pitch():
+    # TODO: Make this return all stocks for a given pitch
+    votes1 = get_all_actions()[0].vote_count
+    votes2 = get_all_actions()[1].vote_count
+
+    return jsonify({"votes1": votes1,
+                    "votes2": votes2 })
+
+@app.route('/vote/<string:ticker>', methods=['GET'])
+def vote(ticker):
+    # TODO: Make this return all stocks for a given pitch
+    # vote_stocks = _filter_unique(stocks, stock_id)
+    vote_stocks = get_pitch_actions(ticker)
+    return render_template('display.html', stocks=vote_stocks)
+
+@app.route('/recieve', methods=['POST'])
+def recieve():
+    if request.method == "POST":
+        number = request.values.get("From")
+
+        # number exists
+        # if number in numbers:
+        #     client.sms.messages.create(to=number, from_=TWILIO_NUM, body='Thanks, but you already voted!')
+        # else:
+        # sweet
+        body = request.values.get('Body').lower()            
+        ticker = body.rsplit(" ", 1)[1]
+        vote = body.rsplit(" ", 1)[0]
+        valid = False
+        for s in stocks:
+            if s["decision"].lower() == vote and s["ticker"].lower() == ticker:
+                s["votes"] += 1
+                numbers.add(number)
+                valid = True
+                client.sms.messages.create(to=number, from_=TWILIO_NUM, body='Thanks for your vote!')
+                break
+        if valid == False:
+            client.sms.messages.create(to=number, from_=TWILIO_NUM, body='That is an invalid vote, please try again!')
+    return jsonify(request.form)
+
+def _filter_unique(stocks, stock_id):
+    vote_stocks = []
+    for s in stocks:
+        if stock_id == s["id"] or s["id"] == stock_id + 1:
+            vote_stocks.append(s)
+    return vote_stocks
--- models.py
+++ models.py
@@ -9,9 +9,8 @@ class Pitch(db.DynamicDocument):
     ''' class to hold the data of a particular pitch the fund is voting on '''
 
     # TODO: add in other field params
-    pitch_date = db.DateTimeField()
-    is_active = db.BooleanField()
-    actions = db.ListField()
+    pitch_date = db.StringField()
+    is_active = db.BooleanField(default=True)
     name = db.StringField(max_length=25)
     ticker = db.StringField(max_length=5)
     created_at = db.DateTimeField(default=datetime.now()) 
@@ -20,11 +19,14 @@ class Action(db.DynamicDocument):
     ''' class to hold the different possible actions of a pitch '''
 
     # TODO: add in other field params
-    vote_count = db.IntField()
+    name = db.StringField(max_length=25)
+    vote_count = db.IntField(default = 0)
     vote_numbers = db.ListField()
     vote_symbol = db.StringField(max_length=1)
+    ticker = db.StringField(max_length=5)
     action = db.StringField(max_length=40)
     amount = db.IntField()
+    action_id = db.IntField()
     created_at = db.DateTimeField(default=datetime.now())
     
 
@@ -32,4 +34,41 @@ class Action(db.DynamicDocument):
 # model layer actions
 # --------------------------------------------
 
-# TODO: add model layer actions
+# TODO: add more model layer actions
+def create_pitch(name, ticker, date):
+    new_pitch = Pitch()
+    new_pitch.name = name
+    new_pitch.ticker = ticker
+    new_pitch.pitch_date = date
+    new_pitch.is_active = True
+    new_pitch.save()
+
+def create_action(symbol, name, action, amount, ticker):
+    # TODO: ADD ATOMIC INCREMENT
+    new_action = Action()
+    new_action.symbol = symbol
+    new_action.name = name
+    new_action.action = action
+    new_action.amount = amount
+    new_action.ticker = ticker
+    new_action.save()
+
+def get_all_pitches():
+    return list(Pitch.objects())
+
+def get_all_actions():
+    return list(Action.objects())
+
+def get_pitch_actions(ticker):
+    # returns pitch data + actions
+    return list(Action.objects(ticker = ticker))
+
+def vote_on_action(ticker, symbol, number):
+    # increments count and adds number to vote
+    
+    pitch_actions = list(Action.objects(ticker = ticker))
+    action = [a for a in pitch_actions if a.symbol == symbol]
+    votes = action.vote_count + 1
+    action.update(set__vote_count = votes)
+    numbers = actions.numbers
+    action.update(set__numbers = action.numbers)
