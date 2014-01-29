5e98aeba5b0ff2f1a2f00c1087c4b5b30fad278d
Evan Casey <evan.casey92@gmail.com>
2014-01-28 19:29:29 -0800

    ok

--- app/views.py
+++ app/views.py
@@ -86,6 +86,7 @@ def recieve():
         else:
             symbol = str(request.json['Body'])
 
+        pdb.set_trace()
         # number exists
         if is_number_voted(symbol,number):
             client.sms.messages.create(to=number, from_=tokens.TWILIO_NUM, body='Thanks, but you already voted!')
